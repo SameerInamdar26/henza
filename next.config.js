a477d25ca5fe9d88d5ba0ca31d244658a0cb7b90
Sam <saminamdar2310@gmail.com>
2025-04-09 18:16:45 +0530

    Fix: Vercel build path issues with alias and CSS

--- next.config.js
+++ next.config.js
@@ -1,1 +1,17 @@
- 
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+      appDir: true,
+    },
+    webpack(config) {
+      config.resolve.alias['@'] = __dirname;
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+   
